29c060932ebf80d1aa274feae54e00d065a119da
Somayeh Rahmani <rahmani.it27@gmail.com>
2025-02-17 21:35:19 +0330

    add react date

--- src/Component/Searchbar/Checkin/Checkin.tsx
+++ src/Component/Searchbar/Checkin/Checkin.tsx
@@ -1,16 +1,50 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./checkin.css";
+import "react-dates/initialize";
+import { DateRangePicker } from "react-dates";
+import moment from "moment";
 
 const Checkin = () => {
   const [opencheckin, setOpenCheckin] = useState(false);
   const [activeOption, setActiveOption] = useState("Dates");
+  const checkinRef = useRef<HTMLDivElement>(null);
+
+  //data picker
+  const [startDate, setStartDate] = useState<moment.Moment | null>(null);
+  const [endDate, setEndDate] = useState<moment.Moment | null>(null);
+  // const [focusedInput, setFocusedInput] = useState<FocusedInputShape | null>(
+  //   null
+  // );
+  const [showDatePicker, setShowDatePicker] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (!checkinRef?.current?.contains(e.target as Node)) {
+        setOpenCheckin(false);
+        setShowDatePicker(false);
+      }
+    };
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div
-      className="checkin_section active_box"
-      onClick={() => setOpenCheckin(true)}
-    >
-      <div className="checkin_title">Check in</div>
-      <input className="checkin_date subtitle_color" placeholder="Add dates" />
+    <div className="checkin_section active_box" ref={checkinRef}>
+      <div
+        className="checkin_title"
+        onClick={() => {
+          setOpenCheckin(true);
+        }}
+      >
+        Check in
+      </div>
+      <input
+        className="checkin_date subtitle_color"
+        placeholder="Add dates"
+        onClick={() => setOpenCheckin(true)}
+      />
 
       {opencheckin && (
         <div className="checkin-container">
@@ -19,14 +53,43 @@ const Checkin = () => {
               <button
                 key={Option}
                 className={`toggle-option ${
-                  activeOption === Option ? "Active" : ""
+                  activeOption === Option ? "active" : ""
                 }`}
-                onClick={() => setActiveOption(Option)}
+                onClick={() => {
+                  setActiveOption(Option);
+                  if (Option === "dates") {
+                    setShowDatePicker(true);
+                  } else {
+                    setShowDatePicker(false);
+                  }
+                }}
               >
                 {Option}
               </button>
             ))}
           </div>
+          {showDatePicker && (
+            <DateRangePicker
+              startDate={startDate}
+              startDateId="start_date_id"
+              endDate={endDate}
+              endDateId="end_Date_id"
+              onDateChange={({
+                startDate,
+                endDate,
+              }: {
+                startDate: moment.Moment | null;
+                endDate: moment.Moment | null;
+              }) => {
+                setStartDate(startDate);
+                setEndDate(endDate);
+              }}
+              // focusedInput={focusedInput}
+              // onFocusChange={(focused) => setFocusedInput(focused)}
+              numberofMonth={12}
+              isOutsideRange={() => false}
+            />
+          )}
         </div>
       )}
     </div>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -5,3 +5,4 @@ declare module "*.png" {
 declare module "*.css";
 declare module "*.svg";
 declare module "*.tsx";
+declare module "react-dates";
